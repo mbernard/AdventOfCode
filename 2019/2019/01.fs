fcbfb4441b1d3e5b801c74e7e88b4e877f3fc327
Miguel Bernard <miguel.bernard@nexusinno.com>
2019-12-01 14:29:20 -0500

    Day 01

---
+++ 2019/2019/01.fs
@@ -0,0 +1,41 @@
+﻿module _01
+
+open Common
+open Xunit
+
+let getFuel x = x / 3 - 2 
+
+let rec getFuel2 x = 
+    getFuel x
+    |> function
+        | y when y > 0 -> 
+            getFuel2 y
+            |> (+) y
+        | _ -> 0 
+            
+let solve1 =
+    "../../../Data/01.txt"
+    |> parseEachLine asInt
+    |> Seq.map getFuel
+    |> Seq.reduce (+)
+
+let solve2 =
+    "../../../Data/01.txt"
+    |> parseEachLine asInt
+    |> Seq.map getFuel2
+    |> Seq.reduce (+)
+    |> printf "%i"
+
+[<Fact>]
+let ``Test solve2`` () =
+    solve2
+    |> printf "%A"
+
+[<Theory>]
+[<InlineData(14, 2)>]
+[<InlineData(1969, 966)>]
+[<InlineData(100756, 50346)>]
+let ``getFuel2 test`` input expected =
+    let actual = getFuel2 input
+    Assert.Equal(expected, actual)
+    
---
+++ 2019/2019/Program.fs
@@ -0,0 +1,6 @@
+
+open _01
+
+module Program = let [<EntryPoint>] main _ = 
+    solve2
+    0
