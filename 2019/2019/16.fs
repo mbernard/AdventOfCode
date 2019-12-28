ed81d3e784663e9222a60cee31b16d2e0de69d49
Miguel Bernard <mig@miguelbernard.com>
2019-12-27 20:35:30 -0500

    day 16 WIP

---
+++ 2019/2019/16.fs
@@ -0,0 +1,33 @@
+﻿module _16
+
+open Common
+open Xunit
+open System.Linq
+
+let generatePattern pattern length =
+    Enumerable.Repeat
+    let pLength = pattern |> Array.length
+    seq {
+        for i in [0 .. length - 1] do
+            yield Array.item (i % pLength) pattern
+    }
+
+let runPhase pattern s phaseNumber =
+    Array.zip pattern s
+    |> Array.map (fun (x,y) -> x * y)
+    |> Array.sum
+
+    s 
+
+let run phase input =
+    let basePattern = generatePattern [|0;1;0;-1|] (input |> Array.length)
+    [1..phase]
+    |> List.fold (runPhase basePattern) input
+    ()
+
+
+[<Theory>]
+[<InlineData("01029498", 4, "12345678")>]
+let ``solve 1`` expected phases input =
+    let actual = ""
+    Assert.Equal(expected, actual)
