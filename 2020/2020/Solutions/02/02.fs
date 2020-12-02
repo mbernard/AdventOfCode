9903dbc511e8abe55743e01f6d9b6c85bc18b6a9
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-02 12:01:57 -0500

    02 - part 1

---
+++ 2020/2020/Solutions/02/02.fs
@@ -0,0 +1,31 @@
+﻿module _02
+
+open Xunit
+open Common
+
+let isValid (args: string[]) =
+    let min = args.[0] |> int
+    let max = args.[1] |> int
+    let c = args.[2] |> char
+    let pass =
+        args.[3].ToCharArray()
+        |> Array.filter (fun x -> x = c)
+        |> Array.length
+    min <= pass && pass <= max
+
+let solve1 data =
+    data
+    |> parseEachLine (withRegex "(\d+)-(\d+) ([a-z]): (.+)")
+    |> Seq.filter isValid
+    |> Seq.length
+    
+
+[<Fact>]
+let ``Solve 1``() =
+    let res = solve1 "../../../Solutions/02/data.txt"
+    Assert.Equal(0, res)
+    
+[<Fact>]
+let ``Solve 1 - Tests 1``() =
+    let res = solve1 "../../../Solutions/02/data-test-1.txt"
+    Assert.Equal(2, res)
