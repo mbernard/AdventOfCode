a49950901386b5fe14b718667b965485aca8b3c3
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-05 15:32:47 -0500

    05 - part 2

--- 2020/2020/Solutions/05/05.fs
+++ 2020/2020/Solutions/05/05.fs
@@ -25,6 +25,29 @@ let solve1 data =
     |> Seq.map split
     |> Seq.map seatId
     |> Seq.max
+    
+let isRealSeat (xs:int seq) (x:int)  =
+    Seq.exists (fun y -> y = (x-1)) xs && 
+    Seq.exists (fun y -> y = (x+1)) xs
+    
+let findSeat xs =
+    let max = Seq.max xs
+    Seq.except xs [0..max]
+    |> Seq.find (isRealSeat xs)
+    
+let solve2 data =
+    data
+    |> parseEachLine asString
+    |> Seq.map toBinary
+    |> Seq.map split
+    |> Seq.map seatId
+    |> findSeat
+    
+[<Fact>]
+let ``Solve 2`` ()=
+    let res = solve2 "../../../Solutions/05/data.txt"
+    Assert.Equal(527, res)
+        
 
 [<Theory>]
 [<InlineData("FBFBBFFRLR")>]
@@ -35,4 +58,4 @@ let ``Solve 1 - Test 1`` s =
 [<Fact>]
 let ``Solve 1`` () =
     let res = solve1 "../../../Solutions/05/data.txt"
-    Assert.Equal(0, res)
+    Assert.Equal(959, res)
