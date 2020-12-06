896ef0d32810b79554faa0adb461f85c2a6c2a53
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-06 10:55:10 -0500

    06 - part 1

---
+++ 2020/2020/Solutions/06/06.fs
@@ -0,0 +1,38 @@
+﻿module _06
+
+open Xunit
+open Common
+
+let assemble (xs: string list, current: string option) (x: string) =
+    if x = "" then
+        let c = current |> Option.get
+        (c :: xs, None)
+    else
+        let c =
+            match current with
+            | None -> x
+            | Some s -> $"{s}{x}"
+            |> Some
+
+        (xs, c)
+        
+let addLastItem (xs,x) = (x |> Option.defaultValue "")::xs
+
+let solve1 data =
+    data
+    |> parseEachLine asString
+    |> Seq.fold assemble ([], None)
+    |> addLastItem
+    |> List.map asCharArray
+    |> List.map (Array.distinct >> Array.length)
+    |> List.sum
+
+[<Fact>]
+let ``Solve 1``() =
+    let res = solve1 "../../../Solutions/06/data.txt"
+    Assert.Equal(6521, res)
+    
+[<Fact>]
+let ``Solve 1 - Example 1``() =
+    let res = solve1 "../../../Solutions/06/data-test-1.txt"
+    Assert.Equal(11, res)
