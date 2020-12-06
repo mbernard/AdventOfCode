99e5264f4183bbc4bab50136d607ad5c6c1dca71
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-06 11:11:42 -0500

    06 - part 2

--- 2020/2020/Solutions/06/06.fs
+++ 2020/2020/Solutions/06/06.fs
@@ -26,7 +26,43 @@ let solve1 data =
     |> List.map asCharArray
     |> List.map (Array.distinct >> Array.length)
     |> List.sum
+    
+let assemble2 (xs: string list list, current: string list) (x: string) =
+    if x = "" then
+        (current :: xs, [])
+    else
+        (xs, x :: current)
+     
+        
+let addLastItem2 (xs,x) = x::xs
 
+let allContains (xs:string list) (x:char) =
+    xs
+    |> List.forall (fun y -> y.Contains(x))
+
+let answeredQuestions xs =
+    ['a'..'z']
+    |> List.map (allContains xs)
+    |> List.sumBy (fun x -> if x then 1 else 0)
+    
+let solve2 data =
+    data
+    |> parseEachLine asString
+    |> Seq.fold assemble2 ([], [])
+    |> addLastItem2
+    |> List.map answeredQuestions
+    |> List.sum
+
+[<Fact>]
+let ``Solve 2``() =
+    let res = solve2 "../../../Solutions/06/data.txt"
+    Assert.Equal(3305, res)
+    
+[<Fact>]
+let ``Solve 2 - Example 1``() =
+    let res = solve2 "../../../Solutions/06/data-test-1.txt"
+    Assert.Equal(6, res)
+    
 [<Fact>]
 let ``Solve 1``() =
     let res = solve1 "../../../Solutions/06/data.txt"
