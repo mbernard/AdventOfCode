a847dba324c8bdd3c241c2150e13e36383da493d
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-11 14:15:39 -0500

    11 - part 2

--- 2020/2020/Solutions/11/11.fs
+++ 2020/2020/Solutions/11/11.fs
@@ -1,5 +1,6 @@
 ﻿module _11
 
+open FSharpx.Collections
 open Xunit
 open Common
 
@@ -53,7 +54,7 @@ let solve1 data =
 [<Fact>]
 let ``Solve 1`` () =
     let res = solve1 "../../../Solutions/11/data.txt"
-    Assert.Equal(0, res)
+    Assert.Equal(2319, res)
 
 [<Fact>]
 let ``Solve 1 - example 1`` () =
@@ -61,3 +62,90 @@ let ``Solve 1 - example 1`` () =
         solve1 "../../../Solutions/11/data-test-1.txt"
 
     Assert.Equal(37, res)
+
+type Direction =
+    | N
+    | NE
+    | E
+    | SE
+    | S
+    | SW
+    | W
+    | NW
+
+let insideArray x y (xs: Position [,]) =
+    x >= 0
+    && y >= 0
+    && x < Array2D.length1 xs
+    && y < Array2D.length2 xs
+
+let rec getFirstSeatInDirection d r c (xs: Position [,]) =
+    let (x, y) =
+        match d with
+        | N -> (r - 1, c)
+        | NE -> (r - 1, c + 1)
+        | E -> (r, c + 1)
+        | SE -> (r + 1, c + 1)
+        | S -> (r + 1, c)
+        | SW -> (r + 1, c - 1)
+        | W -> (r, c - 1)
+        | NW -> (r - 1, c - 1)
+
+    
+    if insideArray x y xs then 
+        match xs.[x, y] with
+        | Floor -> getFirstSeatInDirection d x y xs
+        | z -> Some z
+    else
+        None
+
+let getFirstSeatsInSight r c (xs: Position [,]) =
+    let t =
+          [ getFirstSeatInDirection N r c xs
+            getFirstSeatInDirection NE r c xs
+            getFirstSeatInDirection E r c xs
+            getFirstSeatInDirection SE r c xs
+            getFirstSeatInDirection S r c xs
+            getFirstSeatInDirection SW r c xs
+            getFirstSeatInDirection W r c xs
+            getFirstSeatInDirection NW r c xs ]
+    t
+    |> List.filter (fun x -> x |> Option.isSome)
+    |> List.map Option.get
+    |> List.filter (fun x -> x = OccupiedSeat)
+    |> List.length
+
+let changePosition2 (xs: Position [,]) r c v =
+    let n = getFirstSeatsInSight r c xs
+
+    match v with
+    | EmptySeat when n = 0 -> OccupiedSeat
+    | OccupiedSeat when n >= 5 -> EmptySeat
+    | _ -> v
+
+let rec seatPeople2 (xs: Position [,]) =
+    let ys = Array2D.mapi (changePosition2 xs) xs
+
+    if ys = xs then xs else seatPeople2 ys
+
+let solve2 data =
+    data
+    |> parseEachLine asCharArray
+    |> Seq.toArray
+    |> toArray2D
+    |> seatPeople2
+    |> Seq.cast<Position>
+    |> Seq.filter (fun x -> x = OccupiedSeat)
+    |> Seq.length
+
+[<Fact>]
+let ``Solve 2`` () =
+    let res = solve2 "../../../Solutions/11/data.txt"
+    Assert.Equal(0, res)
+
+[<Fact>]
+let ``Solve 2 - example 1`` () =
+    let res =
+        solve2 "../../../Solutions/11/data-test-1.txt"
+
+    Assert.Equal(26, res)
