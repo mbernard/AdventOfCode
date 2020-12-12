ca3d2e408aefefd2cf02bdcfe59580884b53aaac
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-12 14:07:27 -0500

    12 - part 2

--- 2020/2020/Solutions/12/12.fs
+++ 2020/2020/Solutions/12/12.fs
@@ -16,6 +16,7 @@ type Instruction =
     | L
     | R
 
+type Position = { NS: int; EW: int }
 
 let parse (xs: string array) =
     let i =
@@ -94,3 +95,50 @@ let ``Solve 1 - example 1`` () =
         solve1 "../../../Solutions/12/data-test-1.txt"
 
     Assert.Equal(25, res)
+
+
+let moveWP (wp: Position) d v =
+    match d with
+    | N -> { wp with NS = wp.NS + v }
+    | S -> { wp with NS = wp.NS - v }
+    | E -> { wp with EW = wp.EW + v }
+    | W -> { wp with EW = wp.EW - v }
+
+let rec turnWpLeft90 (ship: Position) (wp: Position) degrees =
+    if degrees = 0 then
+        ship, wp
+    else
+        let wp' = { NS = wp.EW; EW = -wp.NS }
+      
+        turnWpLeft90 ship wp' (degrees - 90)
+
+let execute (ship: Position, wp: Position) (instruction, v) =
+    match instruction with
+    | F ->
+        { NS = ship.NS + wp.NS * v
+          EW = ship.EW + wp.EW * v },
+        wp
+    | L -> turnWpLeft90 ship wp v
+    | R -> turnWpLeft90 ship wp (360 - v)
+    | Direction x -> ship, moveWP wp x v
+
+let solve2 data =
+    data
+    |> parseEachLine (withRegex @"([A-Z])(\d+)")
+    |> Seq.map parse
+    |> Seq.fold execute ({ EW = 0; NS = 0 }, { EW = 10; NS = 1 })
+    |> fst
+    |> (fun ship -> abs ship.EW + abs ship.NS)
+    
+
+[<Fact>]
+let ``Solve 2`` () =
+    let res = solve2 "../../../Solutions/12/data.txt"
+    Assert.Equal(59435, res)
+
+[<Fact>]
+let ``Solve 2 - example 1`` () =
+    let res =
+        solve2 "../../../Solutions/12/data-test-1.txt"
+
+    Assert.Equal(286, res)
