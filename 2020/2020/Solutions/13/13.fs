12b61c2340fa8e85d1586e3f4874ebf62aa84b7c
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-13 16:51:09 -0500

    13 - part 2

--- 2020/2020/Solutions/13/13.fs
+++ 2020/2020/Solutions/13/13.fs
@@ -23,7 +23,7 @@ let rec computeDeparture time bus =
     |> Array.tryFind (fun x -> time % x = 0)
     |> function
     | None -> computeDeparture (time + 1) bus
-    | Some bus -> (bus, time) 
+    | Some bus -> (bus, time)
 
 let solve1 data =
     data
@@ -31,7 +31,7 @@ let solve1 data =
     |> Seq.toArray
     |> parse
     |> (fun x ->
-        let busID,time =
+        let busID, time =
             computeDeparture x.EarliestDeparture x.BusSchedule
 
         (time - x.EarliestDeparture) * busID)
@@ -48,23 +48,67 @@ let ``Solve 1 - example 1`` () =
 
     Assert.Equal(295, res)
 
-//let solve2 data =
-//    data
-//    |> parseEachLine (withRegex @"([A-Z])(\d+)")
-//    |> Seq.map parse
-//    |> Seq.fold execute ({ EW = 0; NS = 0 }, { EW = 10; NS = 1 })
-//    |> fst
-//    |> (fun ship -> abs ship.EW + abs ship.NS)
-//
-//
-//[<Fact>]
-//let ``Solve 2`` () =
-//    let res = solve2 "../../../Solutions/13/data.txt"
-//    Assert.Equal(59435, res)
-//
-//[<Fact>]
-//let ``Solve 2 - example 1`` () =
-//    let res =
-//        solve2 "../../../Solutions/13/data-test-1.txt"
-//
-//    Assert.Equal(286, res)
+let rec diff acc busId x =
+    if acc - x >= 0L
+    then acc - x
+    else diff (acc + busId) busId x 
+
+let toBusRestrictions (data: string) =
+    data.Split(',')
+    |> Array.mapi (fun i x ->
+        match x with
+        | "x" -> (i |> int64,None)
+        | y -> i |> int64, y |> int64 |> Some)
+    |> Array.filter (fun (x,y) -> Option.isSome y)
+    |> Array.map (fun (x,y) ->
+        let busId = Option.get y
+        
+        let diff = diff busId busId x
+        (diff, busId))
+  
+let rec sieve (cs: (int64 * int64) list) (x:int64) (N:int64) =
+    match cs with
+    | [] -> Some(x)
+    | (a,n)::rest ->
+        let arrProgress = Seq.unfold (fun x -> Some(x, x+N)) x
+        let firstXmodNequalA = Seq.tryFind (fun x -> a = x % n)
+        match firstXmodNequalA (Seq.take (n |> int) arrProgress) with
+        | None -> None
+        | Some(x) -> sieve rest x (N*n)
+ 
+
+
+let solve2 data =
+    let cs =
+        data
+        |> parseEachLine asString
+        |> Seq.toArray
+        |> (fun x -> x.[1])
+        |> toBusRestrictions
+        |> Array.toList
+        |> List.map (fun (a,n) -> (a % n, n))
+        |> List.sortBy (snd>>(~-)) 
+    let (a,n)::rest = cs
+    
+    sieve rest a n
+    |> Option.get
+    
+
+[<Fact>]
+let ``Solve 2`` () =
+    let res = solve2 "../../../Solutions/13/data.txt"
+    Assert.Equal(626670513163231L, res)
+
+[<Fact>]
+let ``Solve 2 - example 1`` () =
+    let res =
+        solve2 "../../../Solutions/13/data-test-1.txt"
+
+    Assert.Equal(1068781L, res)
+    
+[<Fact>]
+let ``Solve 2 - example 2`` () =
+    let res =
+        solve2 "../../../Solutions/13/data-test-2.txt"
+
+    Assert.Equal(3417L, res)
