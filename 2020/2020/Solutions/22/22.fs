354dad8bf8da9bf7c3b2ddd4099d10dde715eef6
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-22 23:14:50 -0500

    22 - part 2

--- 2020/2020/Solutions/22/22.fs
+++ 2020/2020/Solutions/22/22.fs
@@ -49,12 +49,11 @@ let ``Solve 1 - example 1`` () =
 
     Assert.Equal(306, res)
 
-let rec play2 gameHistory1 gameHistory2 (decks: int list list) =
+let rec play2 gameHistory (decks: int list list) =
     let deck1 = List.item 0 decks
     let deck2 = List.item 1 decks
 
-    if  List.contains deck1 gameHistory1
-       || List.contains deck2 gameHistory2 then
+    if List.contains decks gameHistory then
         [ deck1; [] ]
     else
         let x :: xs = deck1
@@ -62,7 +61,7 @@ let rec play2 gameHistory1 gameHistory2 (decks: int list list) =
 
         let isPlayerOneWinner =
             if x <= List.length xs && y <= List.length ys then
-                play2 [] [] [ xs |> List.take x; ys |> List.take y ]
+                play2 [] [ xs |> List.take x; ys |> List.take y ]
                 |> List.item 1
                 |> (=) List.Empty
             else
@@ -73,14 +72,14 @@ let rec play2 gameHistory1 gameHistory2 (decks: int list list) =
 
         if newDecks |> List.contains List.Empty
         then newDecks
-        else play2 (deck1 :: gameHistory1) (deck2 :: gameHistory2) newDecks
+        else play2 (decks :: gameHistory) newDecks
 
 let solve2 data =
     data
     |> parseEachLine asString
     |> Seq.toList
     |> parseDecks
-    |> play2 [] []
+    |> play2 []
     |> List.filter (fun d -> d <> List.Empty)
     |> List.exactlyOne
     |> score
@@ -88,8 +87,7 @@ let solve2 data =
 [<Fact>]
 let ``Solve 2`` () =
     let res = solve2 "../../../Solutions/22/data.txt"
-    Assert.Equal(31962, res)
-// 31962
+    Assert.Equal(32054, res)
 
 [<Fact>]
 let ``Solve 2 - example 1`` () =
@@ -97,7 +95,7 @@ let ``Solve 2 - example 1`` () =
         solve2 "../../../Solutions/22/data-test-1.txt"
 
     Assert.Equal(291, res)
-    
+
 [<Fact>]
 let ``Solve 2 - infinite loop prevention`` () =
     let res =
