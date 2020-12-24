5729758be489abed26a1d1397627c7fd6af8e105
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-24 16:33:48 -0500

    24 part 2

--- 2020/2020/Solutions/24/24.fs
+++ 2020/2020/Solutions/24/24.fs
@@ -16,6 +16,10 @@ type Instruction =
     | NW
     | NE
 
+type Color =
+    | White
+    | Black
+
 let rec parseInstructions acc (c :: rest) =
     let i, r =
         match c with
@@ -36,19 +40,18 @@ let rec parseInstructions acc (c :: rest) =
 
     if r = List.Empty then i :: acc else parseInstructions (i :: acc) r
 
-let execute (c, r) ins =
+let getCoord (c, r) ins =
     let isEvenRow = r % 2 = 0
+
     match ins with
-    | E -> (c+1,r)
-    | W -> (c-1,r)
-    | NE -> if isEvenRow then (c+1,r+1) else (c,r+1)
-    | NW -> if isEvenRow then (c,r+1) else (c-1,r+1)
-    | SE -> if isEvenRow then (c+1,r-1) else (c,r-1)
-    | SW -> if isEvenRow then (c,r-1) else (c-1,r-1)     
-    
-let toCoordinates xs =
-    xs
-    |> List.fold execute (0,0)
+    | E -> (c + 1, r)
+    | W -> (c - 1, r)
+    | NE -> if isEvenRow then (c + 1, r + 1) else (c, r + 1)
+    | NW -> if isEvenRow then (c, r + 1) else (c - 1, r + 1)
+    | SE -> if isEvenRow then (c + 1, r - 1) else (c, r - 1)
+    | SW -> if isEvenRow then (c, r - 1) else (c - 1, r - 1)
+
+let toCoordinates xs = xs |> List.fold getCoord (0, 0)
 
 let solve1 data =
     data
@@ -56,7 +59,7 @@ let solve1 data =
     |> Seq.map (Array.toList >> parseInstructions [])
     |> Seq.map toCoordinates
     |> Seq.countBy id
-    |> Seq.filter (fun ((x,y),count) -> count % 2 <> 0)
+    |> Seq.filter (fun ((x, y), count) -> count % 2 <> 0)
     |> Seq.length
 
 [<Fact>]
@@ -70,3 +73,68 @@ let ``Solve 1 - example 1`` () =
         solve1 "../../../Solutions/24/data-test-1.txt"
 
     Assert.Equal(10, res)
+
+let getNeighbors x =
+    [ getCoord x E
+      getCoord x W
+      getCoord x SE
+      getCoord x SW
+      getCoord x NE
+      getCoord x NW ]
+
+let determineColor bt color tile =
+    let blackCount =
+        getNeighbors tile
+        |> List.filter (fun x -> List.contains x bt)
+        |> List.length
+
+    let nextColor =
+        match color with
+        | Black -> if blackCount = 0 || blackCount > 2 then White else Black
+        | White -> if blackCount = 2 then Black else White
+
+    (tile, nextColor)
+
+let flip bt day =
+    let whiteNeighbors =
+        bt
+        |> List.collect getNeighbors
+        |> List.distinct
+        |> List.except bt
+
+    let next1 =
+        whiteNeighbors
+        |> List.map (determineColor bt White)
+
+    let next2 = bt |> List.map (determineColor bt Black)
+
+    next1 @ next2
+    |> List.filter (fun (tile, color) -> color = Black)
+    |> List.map fst
+
+let solve2 data =
+    let blackTiles =
+        data
+        |> parseEachLine asCharArray
+        |> Seq.map (Array.toList >> parseInstructions [])
+        |> Seq.map toCoordinates
+        |> Seq.countBy id
+        |> Seq.filter (fun (_, count) -> count % 2 <> 0)
+        |> Seq.toList
+        |> List.map fst
+
+    [ 1 .. 100 ]
+    |> List.fold flip blackTiles
+    |> List.length
+
+[<Fact>]
+let ``Solve 2`` () =
+    let res = solve2 "../../../Solutions/24/data.txt"
+    Assert.Equal(3667, res)
+
+[<Fact>]
+let ``Solve 2 - example 1`` () =
+    let res =
+        solve2 "../../../Solutions/24/data-test-1.txt"
+
+    Assert.Equal(2208, res)
