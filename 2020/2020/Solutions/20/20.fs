d2c83198ef93a443483027b6475a4c4738e79211
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-12-27 15:56:37 -0500

    20 part 2

--- 2020/2020/Solutions/20/20.fs
+++ 2020/2020/Solutions/20/20.fs
@@ -15,7 +15,7 @@ type Side =
 type Connection =
     { Source: char []
       Side: Side
-      Destination: int64 option }
+      Destinations: int64 list }
 
 type Tile =
     { Number: int64
@@ -80,16 +80,16 @@ let parseTile (x :: lines) =
               Connections =
                   [ { Source = xs.[*, 0]
                       Side = Top
-                      Destination = None }
+                      Destinations = [] }
                     { Source = xs.[9, *]
                       Side = Right
-                      Destination = None }
+                      Destinations = [] }
                     { Source = xs.[*, 9]
                       Side = Bottom
-                      Destination = None }
+                      Destinations = [] }
                     { Source = xs.[0, *]
                       Side = Left
-                      Destination = None } ] })
+                      Destinations = [] } ] })
 
     allTiles
 
@@ -100,7 +100,7 @@ let split lines =
     |> List.chunkBySize 12
     |> List.collect parseTile
 
-let findMatch source tiles side =
+let findMatches source tiles side =
     tiles
     |> List.filter (fun x ->
         let otherC =
@@ -109,7 +109,6 @@ let findMatch source tiles side =
 
         otherC.Source = source)
     |> List.map (fun x -> x.Number)
-    |> List.tryExactlyOne
 
 let connectSide connection otherTiles =
     let otherSide =
@@ -119,7 +118,7 @@ let connectSide connection otherTiles =
         | Right -> Left
         | Left -> Right
 
-    findMatch connection.Source otherTiles otherSide
+    findMatches connection.Source otherTiles otherSide
 
 let findConnections tiles tile =
     let otherTiles =
@@ -130,7 +129,7 @@ let findConnections tiles tile =
         tile.Connections
         |> List.map (fun x ->
             { x with
-                  Destination = connectSide x otherTiles })
+                  Destinations = connectSide x otherTiles })
 
     { tile with Connections = cs }
 
@@ -155,7 +154,7 @@ let solve1 data =
         tiles
         |> List.filter (fun x ->
             x.Connections
-            |> List.filter (fun c -> c.Destination = None)
+            |> List.filter (fun c -> c.Destinations = List.empty)
             |> List.length = 2)
 
     corners
@@ -176,52 +175,111 @@ let ``Solve 1 - example 1`` () =
 
     Assert.Equal(20899048083289L, res)
 
-let isCorner xs = xs |> List.length = 2
-let isBorder xs = xs |> List.length = 3
-//
-//let rec placePiece p x :: rest =
-//
-//    placePiece p rest
-//
-//let solvePuzzle tiles =
-//    let c :: corners =
-//        tiles
-//        |> List.filter (fun x -> x.Position = Corner)
-//
-//    let borders =
-//        tiles
-//        |> List.filter (fun x -> x.Position = Border)
-//
-//    let size =
-//        tiles |> List.length |> float |> Math.Sqrt |> int
-//
-//    let puzzle = Array2D.create size size None
-//
-//    tiles |> List.zip tiles |> Map.ofList
-//
-//    buildBorder c p
-//
-////    placePiece puzzle |> Array2D.map Option.get
-//
-//let solve2 data =
-//    let tiles =
-//        data
-//        |> parseEachLine asString
-//        |> Seq.toList
-//        |> split
-//        |> connectTiles
-//
-//    tiles |> solvePuzzle
-//
-//
-//[<Fact>]
-//let ``Solve 2`` () =
-//    let res = solve1 "../../../Solutions/20/data.txt"
-//    Assert.Equal(0, res)
-//
-//[<Fact>]
-//let ``Solve 2 - example 1`` () =
-//    let res =
-//        solve1 "../../../Solutions/20/data-test-1.txt"
-//
-//    Assert.Equal(273, res)
+let getSide cs side = cs |> List.find (fun x -> x.Side = side)
+
+let findTile leftTile topTile candidate =
+    let leftCondition =
+        match leftTile with
+        | None -> (getSide candidate.Connections Left).Destinations = List.empty
+        | Some l ->
+            (getSide candidate.Connections Left).Destinations |> List.contains l.Number
+            && (getSide l.Connections Right).Destinations |> List.contains candidate.Number
+            
+    let topCondition =
+        match topTile with
+        | None -> (getSide candidate.Connections Top).Destinations = List.empty
+        | Some t ->
+            (getSide candidate.Connections Top).Destinations |> List.contains t.Number
+            && (getSide t.Connections Bottom).Destinations |> List.contains candidate.Number
+    
+    leftCondition && topCondition
+
+let placePiece (puzzle: Tile option [,]) tiles x y =
+    let left =
+        if x = 0 then None else puzzle.[x - 1, y]
+
+    let top =
+        if y = 0 then None else puzzle.[x, y - 1]
+
+    let next = tiles |> List.find (findTile left top)
+    puzzle.[x, y] <- Some next
+
+    ()
+
+let solvePuzzle tiles =
+    let size =
+        tiles
+        |> List.length
+        |> float
+        |> (fun x -> x / 8.0)
+        |> Math.Sqrt
+        |> int
+
+    let puzzle = Array2D.create size size None
+
+    puzzle
+    |> Array2D.iteri (fun x y _ -> placePiece puzzle tiles x y)
+    
+    puzzle
+    |> Array2D.map (fun x -> trimBorders x.Value.Points)
+    |> merge
+    
+let isMonster (sp:char [,]) (m:char[,]) =
+    let mChars = m |> Seq.cast<char>
+    let spChars = sp |> Seq.cast<char>
+    Seq.forall2 (fun m sp -> m = ' ' || m = sp) mChars spChars
+   
+    
+let findMonsters m p =
+    let mutable count = 0
+    for i in [0.. Array2D.length1 p - Array2D.length1 m] do
+        for j in [0.. Array2D.length2 p - Array2D.length2 m] do
+            let subPuzzle = p.[i..i + Array2D.length1 m - 1, j..j+Array2D.length2 m - 1]
+            if isMonster subPuzzle m then count <- count+1
+    count
+    
+let solve2 data =
+    let tiles =
+        data
+        |> parseEachLine asString
+        |> Seq.toList
+        |> split
+        |> connectTiles
+
+    let puzzle = solvePuzzle tiles
+    
+    let monsterCharArray =
+        "../../../Solutions/20/monster.txt"
+        |> parseEachLine asCharArray
+        |> Seq.toArray
+    let monster = Array2D.init 3 20 (fun x y -> monsterCharArray.[x].[y])
+    
+    let ms =
+        puzzle
+        |> configuration
+        |> List.map (findMonsters monster)
+    let maxMonsters =
+        ms
+        |> List.max
+    
+    let rough = 
+        puzzle
+        |> Seq.cast<char>
+        |> Seq.filter (fun x -> x = '#')
+        |> Seq.length
+    
+    rough - maxMonsters * 15
+
+
+
+[<Fact>]
+let ``Solve 2`` () =
+    let res = solve2 "../../../Solutions/20/data.txt"
+    Assert.Equal(2256, res)
+
+[<Fact>]
+let ``Solve 2 - example 1`` () =
+    let res =
+        solve2 "../../../Solutions/20/data-test-1.txt"
+
+    Assert.Equal(273, res)
