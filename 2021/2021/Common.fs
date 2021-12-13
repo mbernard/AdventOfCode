668cc833674050e5a64306c3ab71555482addcaa
Miguel Bernard <migb@google.com>
2021-12-13 12:52:18 -0500

    day 1

---
+++ 2021/2021/Common.fs
@@ -0,0 +1,35 @@
+﻿module Common
+
+open System.IO
+open System
+open System.Text.RegularExpressions
+
+// helper methods for parsing
+let parseFirstLine f (fileName : string) =
+    use fs = new FileStream(fileName, FileMode.Open)
+    use reader = new StreamReader(fs)
+    f (reader.ReadLine())
+
+let parseEachLine f = File.ReadLines >> Seq.map f
+let parseEachLineIndexed f = File.ReadLines >> Seq.mapi f
+
+let asString : string -> string = id
+let asInt : string -> int = int
+let asInt64 : string -> int64 = int64
+let asStringArray : string [] -> string [] = Array.map string
+let asCharArray (x:string) = x.ToCharArray()
+let asIntArray : string [] -> int [] = Array.map int
+let asInt64Array : string [] -> int64 [] = Array.map int64
+let splitBy (c : string) f (str : string) = str.Split([| c |], StringSplitOptions.None) |> f
+let extractInts str = [| for m in Regex.Matches(str, "(-?\d+)") -> int m.Value |]
+let withRegex regex str = [| for m in Regex.Match(str, regex).Groups -> m.Value|] |> Array.tail
+let charsToStr (chars : char seq) = chars |> Seq.map string |> String.concat ""
+let inline debug x =
+#if DEBUG
+    if System.Console.CapsLock then
+        printfn "%A" x
+        // obviously, it must not be necessarily printf;
+        // it can be System.Diagnostics.Debug.WriteLine()
+        // or any other logger tool that exists in the project.
+#endif
+    x
---
+++ 2021/2021/Solutions/01/01.fs
@@ -0,0 +1,16 @@
+﻿module _01
+
+open Common
+open Xunit
+
+let solve1 path =
+  parseEachLine asInt path
+  |> Seq.fold (fun (count:int, prev:int) e ->
+    let c = if e > prev then count + 1 else count
+    (c, e)) (0, System.Int32.MaxValue)
+  |> fst
+
+[<Fact>]
+let ``Solve 1`` () =
+    let res = solve1 "../../../Solutions/01/data.txt"
+    Assert.Equal(1393, res)
