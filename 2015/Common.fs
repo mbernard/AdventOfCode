fe802e61b56cf67cd49555a204f8b00f8350bc94
Miguel Bernard <mig@miguelbernard.com>
2023-06-08 22:06:43 -0400

    day 07

--- 2015/Common.fs
+++ 2015/Common.fs
@@ -31,6 +31,14 @@ let withRegex regex str =
     [| for m in Regex.Match(str, regex).Groups -> m.Value |]
     |> Array.tail
 
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+
+    if m.Success then
+        Some(List.tail [ for g in m.Groups -> g.Value ])
+    else
+        None
+
 let charsToStr (chars: char seq) =
     chars |> Seq.map string |> String.concat ""
 
---
+++ 2015/Solutions/07/07.fs
@@ -0,0 +1,102 @@
+module day07
+
+open System
+open Xunit
+open Common
+open FsUnit
+
+type Operation =
+    | Not of string
+    | Or of string * string
+    | And of string * string
+    | LShift of string * int
+    | RShift of string * int
+    | Set of string
+
+type Value =
+    | Instruction of Operation
+    | Result of uint16
+
+// let parseInstruction (xs: string array) =
+//     match xs[0] with
+//     | Regex "^(\d+)$" [ x ] -> Set(uint16 x)
+//     | Regex "NOT (.*)" [ x ] -> Not x
+//     | Regex "(.*) OR (.*)" [ x; y ] -> Or(x, y)
+//     | Regex "(.*) AND (.*)" [ x; y ] -> And(x, y)
+//     | Regex "(.*) LSHIFT (\d+)" [ x; i ] -> LShift(x, int i)
+//     | Regex "(.*) RSHIFT (\d+)" [ x; i ] -> RShift(x, int i)
+//     | _ -> failwith "Unknow operator"
+//     |> fun op -> { Operation = op; Output = xs[1] }
+
+// let rec execute (map: Map<string, uint16>) x =
+//     let value =
+//         match x.Operation with
+//         | Not y -> ~~~map[y]
+//         | Or (y, z) -> map[y] ||| map[z]
+//         | Set (y) -> y
+//         | And (y, z) -> map[y] &&& map[z]
+//         | LShift (y, z) -> map[y] <<< z
+//         | RShift (y, z) -> map[y] >>> z
+
+//     Map.add x.Output value map
+
+let rec solve (key:string) (map: Map<string,Value>) =
+    if UInt16.TryParse key |> fst then uint16 key,map
+    else
+        match map[key] with
+        | Result x -> x, map
+        | Instruction op -> 
+            let res, resMap =
+                match op with
+                | Not y ->
+                    let v,m = solve y map
+                    ~~~v,m
+                | Or (y, z) -> 
+                    let v,m = solve y map
+                    let v2,m2 = solve z m
+                    v ||| v2, m2
+                | And (y, z) ->
+                    let v,m = solve y map
+                    let v2,m2 = solve z m
+                    v &&& v2, m2
+                | LShift (y, z) ->
+                    let v,m = solve y map
+                    v<<< z, m
+                | RShift (y, z) ->
+                    let v,m = solve y map
+                    v>>> z, m
+                | Set y -> solve y map
+            res, Map.add key (Result res) resMap
+                
+
+let buildGraph (map: Map<string, Value>) (xs: string array) =
+    let value =
+        match xs[0] with
+        | Regex "^(\d+)$" [ x ] -> Result(uint16 x)
+        | Regex "NOT (.*)" [ x ] -> Not(x) |> Instruction
+        | Regex "(.*) OR (.*)" [ x; y ] -> Or(x, y) |> Instruction
+        | Regex "(.*) AND (.*)" [ x; y ] -> And(x, y) |> Instruction
+        | Regex "(.*) LSHIFT (\d+)" [ x; i ] -> LShift(x, int i) |> Instruction
+        | Regex "(.*) RSHIFT (\d+)" [ x; i ] -> RShift(x, int i) |> Instruction
+        | Regex "(.*)" [x] -> Set x |> Instruction
+        | x -> failwithf "Unknow operator '%s'" x
+    
+    Map.add xs[1] value map
+
+[<Fact>]
+let solve1 () =
+    "../../../Solutions/07/data.txt"
+    |> parseEachLine (withRegex "(.*) -> (.*)")
+    |> Seq.fold buildGraph Map.empty
+    |> solve "a"
+    |> fst
+    |> should equal 3176
+
+[<Fact>]
+let solve2 () =
+    "../../../Solutions/07/data2.txt"
+    |> parseEachLine (withRegex "(.*) -> (.*)")
+    |> Seq.fold buildGraph Map.empty
+    |> solve "a"
+    |> fst
+    |> should equal 14710
